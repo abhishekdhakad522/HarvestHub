5be54e3ee3a359e095469ec9017a0c8eca7ca52d
abhishekdhakad522 <abhishekdhakad522@gmail.com>
2026-03-06 19:32:00 +0530

    feat: implement user authentication with registration and login endpoints

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,64 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const registerUser = async (req, res) => {
+    const { username, email, password, role } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+        return res.status(400).json({ message: "Email already in use" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ 
+        username, 
+        email, 
+        password: hashedPassword,
+         role });
+    await user.save();
+
+    const token = jwt.sign({
+         userId: user._id, 
+         role: user.role }, 
+        process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.cookie("token", token);
+
+    res.status(201).json({ message: "User registered successfully" ,
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            role: user.role
+        }
+    });
+}
+
+export const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+        return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+        return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    const token = jwt.sign({
+         userId: user._id, 
+         role: user.role }, 
+        process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.cookie("token", token);
+
+    res.status(200).json({ message: "User logged in successfully" ,
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            role: user.role
+        }
+    });
+}
---
+++ backend/src/routes/auth.routes.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import { registerUser, loginUser } from '../controllers/auth.controller.js';
+const router = express.Router();
+
+router.post('/user/register', registerUser);
+router.post('/user/login', loginUser);
+
+export default router;
